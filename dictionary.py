cff81d539b9dc7f57bee18a278f4f56bd1895487
Joseph Baturiano <josephbaturiano23@gmail.com>
2022-11-12 21:36:43 +0700

    Display a menu

--- dictionary.py
+++ dictionary.py
@@ -1,2 +1,9 @@
-print("programmed by joseph")
-print("BSCOE 2-2")
+print("\n\t*****PROGRAMMED BY*****")
+print("\t***BATURIANO, JOSEPH Z.***")
+print("\t******BSCOE 2-2******\n")
+
+print("\n\t=========MENU=========")
+print("\n\t1 -> Add an item\n"
+      "\n\t2 -> Search\n"
+      "\n\t3 -> Exit (y/n)")
+print("\n\t======================")
