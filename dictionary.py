ac0e00cba912647bac18d398a10c13705981dcfb
Joseph Baturiano <josephbaturiano23@gmail.com>
2022-11-13 09:40:39 +0700

    fix bugs in option 1 and perform option 2

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,6 @@
 peopleInfo = {}
+Alluser = []
+
 print("\n\t*****PROGRAMMED BY*****")
 print("\t***BATURIANO, JOSEPH Z.***")
 print("\t******BSCOE 2-2******\n")
@@ -7,9 +9,10 @@ print("\t******BSCOE 2-2******\n")
 def menu():
 
     print("\n\t=========MENU=========")
-    print("\n\t1 -> Add an item\n"
-          "\n\t2 -> Search\n"
-          "\n\t3 -> Exit (y/n)")
+    print("\n\t1 ---> Add an item\n"
+          "\n\t2 ---> Search\n"
+          "\n\t3 ---> View all contacts\n"
+          "\n\t4 ---> Exit (y/n)")
     print("\n\t======================")
 
 
@@ -17,22 +20,49 @@ menu()
 
 
 def add():
-    Name = input("\tEnter your Full name: ").capitalize()
-    Address = str(input("\tEnter your address: ")).capitalize()
-    peopleInfo[Name] = Address
-    Age = input("\tEnter your age: ")
-    peopleInfo[Name] = Age
-    Phone_no = int(input("\tEnter your phone number: "))
-    peopleInfo[Name] = Phone_no
-    bloodType = input("\tEnter your Blood Type: ")
-    peopleInfo[Name] = bloodType
-    print("Information Saved!")
+    peopleInfo = {}
+    Nameval = input("\n\tEnter your Fullname: ").capitalize()
+    Addressval = input("\tEnter your Address: ").capitalize()
+    Ageval = int(input("\tEnter your age: "))
+    Phone_noVal = int(input("\tEnter your phone number: "))
+    bloodTypeval = input("\tEnter your Blood Type: ").upper()
+    peopleInfo["Name"] = Nameval
+    peopleInfo["Address"] = Addressval
+    peopleInfo["Age"] = Ageval
+    peopleInfo["Phone_number"] = Phone_noVal
+    peopleInfo["BloodType"] = bloodTypeval
+    Alluser.append(peopleInfo)
+    print("\n<<<<<<<<<<<Information Saved!>>>>>>>>>>>")
+
+
+def search():
+    search = input("\n\tEnter your Full name: ").capitalize()
+    for i in range(len(Alluser)):
+        peopleInfo = Alluser[i]
+        keys_ls = []
+        for key, val in peopleInfo.items():
+            keys_ls.append(key)
+        for j in range(len(keys_ls)):
+            key = keys_ls[j]
+            if search == peopleInfo[key]:
+                print("\t\n<<<<<<<<<<<<This is the information of",
+                      search, ">>>>>>>>>>>>>>>>>>\n")
+                print("\n\tFull Name:", search)
+                print("\tAddress: ", peopleInfo["Address"])
+                print("\tAge: ", peopleInfo["Age"])
+                print("\tPhone Number: ", peopleInfo["Phone_number"])
+                print("\tBlood Type: ", peopleInfo["BloodType"])
 
 
 # Allow user to select item in the menu (check if valid)
-choice = int(input("Enter a choice from menu: "))
+choice = int(input("\nEnter a choice from menu: "))
 # Perform the selected option
 while True:
     if choice == 1:
         add()
         menu()
+        choice = int(input("\nEnter a choice from menu: "))
+    if choice == 2:
+        search()
+        menu()
+        choice = int(input("\nEnter a choice from menu: "))
