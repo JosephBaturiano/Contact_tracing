9ae6eabd9004111c04935e7db10b5013ca446ef1
Joseph Baturiano <josephbaturiano23@gmail.com>
2022-11-13 10:46:08 +0700

    perform option 3&4, also finalizing design

--- dictionary.py
+++ dictionary.py
@@ -9,10 +9,10 @@ print("\t******BSCOE 2-2******\n")
 def menu():
 
     print("\n\t=========MENU=========")
-    print("\n\t1 ---> Add an item\n"
-          "\n\t2 ---> Search\n"
-          "\n\t3 ---> View all contacts\n"
-          "\n\t4 ---> Exit (y/n)")
+    print("\n\t1 -> Add an item\n"
+          "\n\t2 -> Search\n"
+          "\n\t3 -> View all contacts\n"
+          "\n\t4 -> Exit (y/n)")
     print("\n\t======================")
 
 
@@ -21,7 +21,7 @@ menu()
 
 def add():
     peopleInfo = {}
-    Nameval = input("\n\tEnter your Fullname: ").capitalize()
+    Nameval = input("\tEnter your Fullname: ").capitalize()
     Addressval = input("\tEnter your Address: ").capitalize()
     Ageval = int(input("\tEnter your age: "))
     Phone_noVal = int(input("\tEnter your phone number: "))
@@ -32,11 +32,11 @@ def add():
     peopleInfo["Phone_number"] = Phone_noVal
     peopleInfo["BloodType"] = bloodTypeval
     Alluser.append(peopleInfo)
-    print("\n<<<<<<<<<<<Information Saved!>>>>>>>>>>>")
+    print("<<<<<<<<<<<Information Saved!>>>>>>>>>>>")
 
 
 def search():
-    search = input("\n\tEnter your Full name: ").capitalize()
+    search = input("\tEnter your Full name: ").capitalize()
     for i in range(len(Alluser)):
         peopleInfo = Alluser[i]
         keys_ls = []
@@ -45,19 +45,30 @@ def search():
         for j in range(len(keys_ls)):
             key = keys_ls[j]
             if search == peopleInfo[key]:
-                print("\t\n<<<<<<<<<<<<This is the information of",
-                      search, ">>>>>>>>>>>>>>>>>>\n")
-                print("\n\tFull Name:", search)
-                print("\tAddress: ", peopleInfo["Address"])
-                print("\tAge: ", peopleInfo["Age"])
-                print("\tPhone Number: ", peopleInfo["Phone_number"])
-                print("\tBlood Type: ", peopleInfo["BloodType"])
+                print("\nHere are the information of", search)
+                print("\nFull Name:", search)
+                print("Address: ", peopleInfo["Address"])
+                print("Age: ", peopleInfo["Age"])
+                print("Phone Number: ", peopleInfo["Phone_number"])
+                print("Blood Type: ", peopleInfo["BloodType"])
+
+
+def display_all():
+    print("\n\tHERE ARE THE CONTACTS")
+    for i in range(len(Alluser)):
+        show = Alluser[i]
+        print("\n\tFullname: ", show["Name"])
+        print("\tAddress: ", show["Address"])
+        print("\tAge: ", show["Age"])
+        print("\tPhone number: ", show["Phone_number"])
+        print("\tBlood Type: ", show["BloodType"])
 
 
 # Allow user to select item in the menu (check if valid)
 choice = int(input("\nEnter a choice from menu: "))
 # Perform the selected option
-while True:
+ans = "y"
+while ans == "y":
     if choice == 1:
         add()
         menu()
@@ -66,3 +77,21 @@ while True:
         search()
         menu()
         choice = int(input("\nEnter a choice from menu: "))
+    if choice == 3:
+        display_all()
+        ans = input("\nGo back to menu(y/n): ")
+        if ans == "y":
+            menu()
+            choice = int(input("\nEnter a choice from menu: "))
+        else:
+            break
+    if choice == 4:
+        leave = input(
+            "\n\tAre you sure you want to leave this program?(y/n): ").lower()
+        if leave == "n":
+            menu()
+            choice = int(input("\nEnter a choice from menu: "))
+        else:
+            print(
+                "\n===========Thanks for using this program! Have a nice day!=============")
+            break
